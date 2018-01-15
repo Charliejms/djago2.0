d2f55e27d2cd1a6272673a00eef7c1bf6908e773
charliejms <charlie.jmsklein@gmail.com>
2018-01-15 17:49:34 +0100

    Custon Admin Django

--- photos/admin.py
+++ photos/admin.py
@@ -3,9 +3,31 @@ from photos.models import Photo
 
 
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ['name', 'owner', 'visibility', 'description', 'license']
+    list_display = ['name', 'owner_name', 'visibility', 'description', 'license']
     list_display_links = ['name']
+    list_filter = ['license', 'visibility']
     search_fields = ['name']
+
+    def owner_name(self, obj):
+        return obj.owner.first_name + obj.owner.last_name
+
+    owner_name.short_description = 'Owner'
+    owner_name.admin_order_field = 'owner'
+
+    fieldsets = (
+        (None, {
+            'fields': ('name',),
+            'classes': ('wide',)
+        }),
+        ('Description & Author', {
+            'fields': ('description', 'owner'),
+            'classes': ('wide',)
+        }),
+        ('Extra', {
+            'fields': ('url', 'license', 'visibility'),
+            'classes': ('wide', 'collapse',)
+        }),
+    )
 
     class Meta:
         model = Photo
