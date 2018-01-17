d30707b64f7986184a195198a6ddca63826fb9b9
charliejms <charlie.jmsklein@gmail.com>
2018-01-17 12:54:21 +0100

    APi de borrado de usuarios

--- users/api/views.py
+++ users/api/views.py
@@ -53,3 +53,8 @@ class UserDetailAPI(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, username):
+        user = get_object_or_404(User, username=username)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
