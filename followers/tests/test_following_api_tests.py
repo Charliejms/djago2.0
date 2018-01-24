f21090e415eceb0f8db1af27f5db3b16899bf8e0
charliejms <charlie.jmsklein@gmail.com>
2018-01-24 23:05:47 +0100

    Refactoring tests

--- followers/tests/test_following_api_tests.py
+++ followers/tests/test_following_api_tests.py
@@ -1,37 +1,11 @@
 from django.contrib.auth.models import User
-from django.test import TestCase, override_settings
+from django.test import override_settings
 
 # Create your tests here.
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from followers.models import Relationship
-from followers.utils import get_followers, get_followers_v2, get_following
-
-
-class RelationshipTests(TestCase):
-
-    USER_PASSWORD = 'Django13'
-
-    def setUp(self):
-        """
-        Se ejecuta antes de cada test
-        :return: Objeto de pruebas
-        """
-        self.user1 = User.objects.create_user('javiercs', 'javier.cacuango@gmail.com', self.USER_PASSWORD)
-        self.user2 = User.objects.create_user('mariagb', 'maria.garrido.boscana@gmail.com', self.USER_PASSWORD)
-
-        Relationship.objects.create(origin=self.user1, target=self.user2)
-
-    def test_get_followers_returns_users_that_follow_by_a_given_user(self):
-        followers = get_followers_v2(self.user2)
-
-        self.assertEquals([self.user1], followers) # followers ==[user1]
-
-    def test_get_following_returns_users_that_a_given_user_follows(self):
-        following = get_following(self.user1)
-
-        self.assertEquals([self.user2], following)
 
 
 @override_settings(ROOT_URLCONF='followers.urls')
@@ -42,6 +16,7 @@ class APITests(APITestCase):
 
     def setUp(self):
         # setup
+        self.user0 = User.objects.create_user('firstuser', 'firstuser@gmail.com', self.USER_PASSWORD)
         self.user1 = User.objects.create_user('javiercs', 'javier.cacuango@gmail.com', self.USER_PASSWORD)
         self.user2 = User.objects.create_user('mariagb', 'maria.garrido.boscana@gmail.com', self.USER_PASSWORD)
         self.user3 = User.objects.create_user('charliedev', 'charliedev@gmail.com', self.USER_PASSWORD)
@@ -50,7 +25,6 @@ class APITests(APITestCase):
         Relationship.objects.create(origin=self.user1, target=self.user2)
         Relationship.objects.create(origin=self.user1, target=self.user3)
         Relationship.objects.create(origin=self.user1, target=self.user4)
-
 
     def test_following_users_endpoint_fails_when_user_is_not_authenticated(self):
 
@@ -61,13 +35,13 @@ class APITests(APITestCase):
 
     def test_user_doesnot_follow_any_user_and_return_empty_list_is_returned(self):
         # authenticate user1
-        self.client.login(username=self.user2.username, password=self.USER_PASSWORD)
+        self.client.login(username=self.user0.username, password=self.USER_PASSWORD)
         # action
         response = self.client.get(self.FOLLOWING_API_URL)
         # assert response 200 Ok
         self.assertEquals(response.status_code, status.HTTP_200_OK)
         # assert data is empty
-        self.assertEquals(len(response.data), 0)
+        self.assertEquals(len(response.data['results']), 0)
 
     def test_user_follows_3_users_and_3_users_are_returned(self):
 
@@ -78,4 +52,4 @@ class APITests(APITestCase):
 
         self.assertEquals(response.status_code, status.HTTP_200_OK)
         # assert data is empty
-        self.assertEquals(len(response.data), 3)
+        self.assertEquals(response.data['count'], 3)
